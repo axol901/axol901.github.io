ba0e0445a0de279e72d640743add0832bcbd846b
axol901 <nyx901@gmail.com>
2024-03-04 14:58:26 -0500

    Move script to external file

---
+++ contactform.js
@@ -0,0 +1,12 @@
+//on submitting the form, hide the contact form and display the submission message, then update the hasSubmitted variable
+			
+			var hasSubmitted = 0;
+			
+			function subMessage()
+			{
+				if (hasSubmitted > 0)
+				{
+					document.getElementById('submessage').style.display='block';
+					document.getElementById('contactform').style.display='none';
+				}
+			}
