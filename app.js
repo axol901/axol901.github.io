f44c042bbdf1ecb717d1ab2d7f061e66974c018c
axol901 <nyx901@gmail.com>
2024-04-12 13:16:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// make necessary elements or files accessible
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
+// send appropriate html files when client navigates to certain urls
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
@@ -18,6 +20,7 @@ app.get('/contact.html', (req, res) => {
     res.sendFile(__dirname + '/contact.html');
 });
 
+// when submitting form, assign content of form elements to server-side variables and print them to the console
 app.post('/submit-form', (req, res) => {
     const fname = req.body.fname;
     const lname = req.body.lname;
@@ -29,7 +32,7 @@ app.post('/submit-form', (req, res) => {
     console.log('Last Name: '+lname);
     console.log('Email: '+email);
     console.log('Message: '+message);
-    
+
     res.sendFile(__dirname + '/contact.html');
 });
 
